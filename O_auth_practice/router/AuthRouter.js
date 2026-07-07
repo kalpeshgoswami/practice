74b1a4543b3b4a35133f882d6f79daa7cae6ab40
Kalpesh Goswami <kalpeshgoswami298@gmail.com>
2026-07-07 10:54:14 +0530

    add views and router

--- O_auth_practice/router/AuthRouter.js
+++ O_auth_practice/router/AuthRouter.js
@@ -1,12 +1,37 @@
 
-// import express from "express";
-// import passport from "passport";
+import express from "express";
+import passport from "passport";
 
-// import HttpError from "../middleware/HttpError.js";
+import HttpError from "../middleware/HttpError.js";
 
-// const router = express.Router();
+const router = express.Router();
 
-// router.get("/loggin", (req, res) => {
-//     res.render("loggin");
-// });
+router.get("/loggin", (req, res) => {
+    res.render("loggin");
+});
 
+router.get(
+    "/google/redirect",
+    passport.authenticate("google", {
+        scope: ["profile", "email"]
+    })
+);
+
+router.get(
+    "/google/redirect",
+    passport.authenticate("google", { failureRedirect: "/" }),
+    (req, res) => {
+        res.redirect("/profile");
+    }
+);
+
+router.get("/logout", (req, res) => {
+    req.logout((err) => {
+        if (err) {
+            throw new Error("error")
+        }
+        res.redirect("/")
+    })
+})
+
+export default router;
